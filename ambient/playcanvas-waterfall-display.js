caa6b7de33c4c7a685a389751a4939bc491d3e7a
Lloyd Evans <contact@lloydevans.co.uk>
2020-09-03 14:46:35 +0100

    Update build

--- ambient/playcanvas-waterfall-display.js
+++ ambient/playcanvas-waterfall-display.js
@@ -52,25 +52,19 @@
             var _this = _super !== null && _super.apply(this, arguments) || this;
             _this.material = new pc.StandardMaterial();
             _this.graphNode = new pc.GraphNode();
+            _this.binVector = new pc.Vec3();
+            _this.quat = new pc.Quat();
             return _this;
         }
         WaterfallDisplay.prototype.initialize = function () {
             var _this = this;
             this.on("attr:fftSize", function () {
                 _this.analyzerNode.fftSize = _this.fftSize;
+                _this.binCount = _this.analyzerNode.frequencyBinCount;
                 _this.reset();
             });
             this.on("attr:smoothingTimeConstant", function () {
                 _this.analyzerNode.smoothingTimeConstant = _this.smoothingTimeConstant;
-            });
-            this.on("attr:scaleX", function () {
-                _this.calculateExtents();
-            });
-            this.on("attr:speed", function () {
-                _this.calculateExtents();
-            });
-            this.on("attr:startOffset", function () {
-                _this.calculateExtents();
             });
             this.on("attr:length", function () {
                 _this.reset();
@@ -91,17 +85,27 @@
             }
             if (this.active) {
                 this.addWindow();
-                this.calculateColors();
+                if (this.fadeDecay !== 1) {
+                    for (var i = 0; i < this.colors.length / 4; i++) {
+                        this.colors[i * 4 + 3] *= this.fadeDecay;
+                    }
+                }
+                for (var i = this.binCount; i < this.vertices.length / 3; i++) {
+                    this.vertices[3 * i + 0] += this.velocity.x;
+                    this.vertices[3 * i + 1] += this.velocity.y;
+                    this.vertices[3 * i + 2] += this.velocity.z;
+                }
                 this.updateMesh();
             }
         };
         WaterfallDisplay.prototype.play = function (slotName) {
+            var _a, _b;
             if (!this.active) {
                 this.active = true;
             }
             this.entity.sound.stop();
             this.playbackInstance = this.entity.sound.play(slotName);
-            this.playbackInstance._connectorNode.connect(this.analyzerNode);
+            (_b = (_a = this.playbackInstance) === null || _a === void 0 ? void 0 : _a._connectorNode) === null || _b === void 0 ? void 0 : _b.connect(this.analyzerNode);
         };
         WaterfallDisplay.prototype.stop = function () {
             this.entity.sound.stop();
@@ -115,41 +119,62 @@
             this.bins = new Float32Array(area);
             this.vertices = new Float32Array(3 * area);
             this.uvs = new Float32Array(2 * area);
-            this.colors = new Float32Array(4 * area);
-            this.calculateExtents();
+            this.colors = new Float32Array(4 * area).fill(1, 0, 4 * area);
             this.calculateTriangles();
             this.calculateUvs();
-            this.calculateColors();
             this.createMeshInstance();
         };
         WaterfallDisplay.prototype.addWindow = function () {
             this.analyzerNode.getFloatFrequencyData(this.fftWindowData);
+            var len = this.fftWindowData.length;
             for (var i = 0; i < this.fftWindowData.length; i++) {
-                this.fftWindowData[i] =
-                    (this.fftWindowData[i] + NOISE_FLOOR + this.gain) / NOISE_FLOOR;
+                this.fftWindowData[i] = (this.fftWindowData[i] + NOISE_FLOOR + this.gain) / NOISE_FLOOR;
                 this.fftWindowData[i] *= this.amp;
             }
-            var len = this.fftWindowData.length;
-            for (var i = this.bins.length - 1 - len; i > 0; i--) {
+            for (var i = this.bins.length - 1 - len; i >= 0; i--) {
                 this.bins[i + len] = this.bins[i];
+            }
+            for (var i = this.colors.length - 1 - len * 4; i >= 0; i--) {
+                this.colors[i + len * 4] = this.colors[i];
+            }
+            for (var i = this.vertices.length - 1 - len * 3; i >= 0; i--) {
+                this.vertices[i + len * 3] = this.vertices[i];
             }
             for (var i = 0; i < len; i++) {
                 this.bins[i] = this.fftWindowData[i];
             }
-            for (var i = this.vertices.length / 3 - len; i < this.vertices.length / 3; i++) {
-                this.vertices[i * 3 + 1] = 0;
+            for (var i = 0; i < len; i++) {
+                color.set(0, 0, 0, 0);
+                switch (this.colorMode) {
+                    case "amp":
+                        color.lerp(this.colorMin, this.colorMax, Math.max(0, Math.min(1, this.fftWindowData[i] / this.amp)));
+                        break;
+                    case "freq":
+                        color.lerp(this.colorMin, this.colorMax, Math.max(0, i / len));
+                        break;
+                }
+                this.colors[i * 4 + 0] = color.r;
+                this.colors[i * 4 + 1] = color.g;
+                this.colors[i * 4 + 2] = color.b;
+                this.colors[i * 4 + 3] = color.a;
             }
             for (var i = 0; i < len; i++) {
-                this.vertices[i * 3 + 1] = 0;
-            }
-            for (var i = len; i < this.vertices.length / 3 - len; i++) {
-                this.vertices[i * 3 + 1] = Math.max(0, this.bins[i]);
+                var scale = this.width / len;
+                this.binVector.x = i * scale - this.width / 2;
+                this.binVector.y = Math.max(0, this.fftWindowData[i]);
+                this.binVector.z = 0;
+                this.quat.setFromEulerAngles(this.rotation.x, this.rotation.y, this.rotation.z);
+                this.quat.transformVector(this.binVector, this.binVector);
+                this.vertices[i * 3 + 0] = this.binVector.x + this.position.x;
+                this.vertices[i * 3 + 1] = this.binVector.y + this.position.y;
+                this.vertices[i * 3 + 2] = this.binVector.z + this.position.z;
             }
         };
         WaterfallDisplay.prototype.updateMesh = function (firstUpdate) {
             if (firstUpdate === void 0) { firstUpdate = false; }
             this.mesh.setPositions(this.vertices);
             this.mesh.setColors(this.colors);
+            this.calculateTriangles();
             this.mesh.setNormals(pc.calculateNormals(this.vertices, this.triangles));
             if (firstUpdate) {
                 this.mesh.setUvs(0, this.uvs);
@@ -163,6 +188,7 @@
             this.updateMesh(true);
             this.graphNode = new pc.GraphNode();
             this.meshInstance = new pc.MeshInstance(this.graphNode, this.mesh, this.material);
+            this.meshInstance.renderStyle = pc.RENDERSTYLE_SOLID;
             if (!this.entity.model) {
                 this.entity.addComponent("model");
             }
@@ -170,47 +196,6 @@
             model.graph = this.graphNode;
             model.meshInstances = [this.meshInstance];
             this.entity.model.model = model;
-        };
-        WaterfallDisplay.prototype.calculateColors = function () {
-            var wL = this.fftWindowData.length;
-            for (var i = this.colors.length - 1 - wL * 4; i > 0; i--) {
-                this.colors[i + wL * 4] = this.colors[i];
-            }
-            for (var i = 0; i < wL; i++) {
-                color.set(0, 0, 0, 0);
-                color.lerp(this.colorMin, this.colorMax, Math.max(0, Math.min(1, this.bins[i] / this.amp)));
-                this.colors[i * 4 + 0] = color.r;
-                this.colors[i * 4 + 1] = color.g;
-                this.colors[i * 4 + 2] = color.b;
-                this.colors[i * 4 + 3] = i === 0 || i === this.binCount - 1 ? 0 : color.a;
-            }
-            if (this.fadeDecay !== 1) {
-                for (var x = 0; x < this.binCount; x++) {
-                    for (var y = 0; y < this.length; y++) {
-                        var i = x + y * this.binCount;
-                        this.colors[i * 4 + 3] *= this.fadeDecay;
-                    }
-                }
-            }
-        };
-        WaterfallDisplay.prototype.calculateExtents = function () {
-            var scaleX = this.scaleX / this.binCount;
-            for (var z = 0; z < this.length; z++) {
-                for (var x = 0; x < this.binCount; x++) {
-                    var i = x + z * this.binCount;
-                    this.vertices[3 * i] = (-x + this.binCount / 2) * scaleX;
-                    if (z === 0) {
-                        this.vertices[3 * i + 2] = 0;
-                    }
-                    else if (z === 1) {
-                        this.vertices[3 * i + 2] = this.startOffset * 2;
-                    }
-                    else {
-                        var referenceSpeed = 256 / this.analyzerNode.frequencyBinCount;
-                        this.vertices[3 * i + 2] = z * this.speed * referenceSpeed;
-                    }
-                }
-            }
         };
         WaterfallDisplay.prototype.calculateUvs = function () {
             for (var z = 0; z < this.length; z++) {
@@ -223,9 +208,9 @@
         };
         WaterfallDisplay.prototype.calculateTriangles = function () {
             this.triangles = [];
-            for (var x = 0; x < this.length - 1; x++) {
-                for (var y = 0; y < this.binCount - 1; y++) {
-                    this.triangles.push(x * this.binCount + y + 1, (x + 1) * this.binCount + y, x * this.binCount + y, (x + 1) * this.binCount + y, x * this.binCount + y + 1, (x + 1) * this.binCount + y + 1);
+            for (var x = 0; x < this.binCount - 1; x++) {
+                for (var y = 0; y < this.length - 1; y++) {
+                    this.triangles.push(x + 1 + y * this.binCount, x + (y + 1) * this.binCount, x + y * this.binCount, x + (y + 1) * this.binCount, x + 1 + y * this.binCount, x + 1 + (y + 1) * this.binCount);
                 }
             }
         };
@@ -254,6 +239,24 @@
         ],
         default: 256,
     });
+    WaterfallDisplay.attributes.add("position", {
+        description: "Spawn point for new windows.",
+        title: "Position",
+        type: "vec3",
+        default: [0, 0, 0],
+    });
+    WaterfallDisplay.attributes.add("rotation", {
+        description: "Spawn rotation.",
+        title: "Rotation",
+        type: "vec3",
+        default: [0, 0, 0],
+    });
+    WaterfallDisplay.attributes.add("velocity", {
+        description: "Static velocity.",
+        title: "Velocity",
+        type: "vec3",
+        default: [0, 0, 0],
+    });
     WaterfallDisplay.attributes.add("smoothingTimeConstant", {
         description: "The smoothingTimeConstant property of the AnalyserNode interface is a double value representing the averaging constant with the last analysis frame.",
         title: "Smoothing",
@@ -262,14 +265,13 @@
         min: 0,
         max: 1,
     });
-    WaterfallDisplay.attributes.add("scaleX", {
-        description: "Mesh X axis extent.",
-        title: "Scale X",
+    WaterfallDisplay.attributes.add("width", {
+        description: "Trail width extent.",
+        title: "Width",
         type: "number",
-        default: 16,
-        min: 1,
-        max: 32,
-        precision: 0.1,
+        default: 1,
+        min: 0,
+        max: 64,
     });
     WaterfallDisplay.attributes.add("amp", {
         description: "Mesh Z axis extent.",
@@ -277,7 +279,7 @@
         type: "number",
         default: 1,
         min: 0.01,
-        max: 4,
+        max: 10,
     });
     WaterfallDisplay.attributes.add("speed", {
         description: "The rate of Z axis movement over time.",
@@ -300,7 +302,7 @@
         title: "Length",
         type: "number",
         default: 128,
-        min: 4,
+        min: 1,
         max: 256,
         precision: 0.1,
     });
@@ -312,31 +314,30 @@
         min: 0.9,
         max: 1,
     });
-    WaterfallDisplay.attributes.add("startOffset", {
-        description: "Amount of gain between start vertices and first fft window.",
-        title: "Start Offset",
-        type: "number",
-        default: 0.5,
-        min: 0,
-        max: 1,
+    WaterfallDisplay.attributes.add("colorMax", {
+        description: "Vertice color (max).",
+        title: "Color Max",
+        type: "rgba",
+        default: [1, 0, 0, 1],
     });
     WaterfallDisplay.attributes.add("colorMin", {
         description: "Vertice color (min).",
-        title: "Color Low",
-        type: "rgba",
-        default: [0, 1, 0, 1],
-    });
-    WaterfallDisplay.attributes.add("colorMax", {
-        description: "Vertice color (max).",
         title: "Color Min",
         type: "rgba",
-        default: [1, 0, 0, 1],
+        default: [0, 1, 0, 1],
     });
     WaterfallDisplay.attributes.add("cycleColors", {
         description: "Easter egg.",
         title: "Cycle Color",
         type: "boolean",
         default: false,
+    });
+    WaterfallDisplay.attributes.add("colorMode", {
+        description: "",
+        title: "Color Mode",
+        type: "string",
+        enum: [{ Amp: "amp" }, { Frequency: "freq" }],
+        default: "amp",
     });
     WaterfallDisplay.attributes.add("materialAsset", {
         description: "Material asset.",
